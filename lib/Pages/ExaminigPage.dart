84a2c2a494299467aef217afab48281a8f59820c
Mohsen Meshkini <mmeshkiny@yahoo.com>
2018-10-03 19:13:11 +0330

    Create Draft Pages Learning , Examining and Reviewing

---
+++ lib/Pages/ExaminigPage.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ExaminingPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new ExaminingPageState();
+  }
+}
+
+class ExaminingPageState extends State<ExaminingPage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("Examining");
+  }
+}
---
+++ lib/Pages/ReviewingPage.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ReviewingPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new ReviewingPageState();
+  }
+}
+
+class ReviewingPageState extends State<ReviewingPage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("Reviewing");
+  }
+}
