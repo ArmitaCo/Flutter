2709309d6f8fcdfbffdb2d4293b4f1a3af410869
axmxixr <undead.warlockk@gmail.com>
2018-10-06 16:18:36 +0330

    routes from Boxes to learning examing and reviewing pages set Icons for Boxes https://github.com/ArmitaCo/Flutter/projects/1#card-13628872 https://github.com/ArmitaCo/Flutter/projects/1#card-13629320

--- lib/Pages/SelectedPackage.dart
+++ lib/Pages/SelectedPackage.dart
@@ -2,6 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app_rote/Model/PackageBoxModel.dart';
 import 'package:flutter_app_rote/Model/PackageModel.dart';
+import 'package:flutter_app_rote/Pages/ExaminingPage.dart';
+import 'package:flutter_app_rote/Pages/LearningPage.dart';
+import 'package:flutter_app_rote/Pages/ReviewingPage.dart';
 import 'package:flutter_app_rote/Tools/Authentication.dart';
 
 class SelectedPackage extends StatefulWidget {
@@ -32,7 +35,7 @@ class MySelectedPackage extends State<SelectedPackage> {
     });
   }
 
-
+  int BoxState;
 
   @override
   Widget build(BuildContext context) {
@@ -49,7 +52,7 @@ class MySelectedPackage extends State<SelectedPackage> {
           ]),
       body: Padding(
           padding: EdgeInsets.all(10.0),
-          child:  Column(
+          child: Column(
             textDirection: TextDirection.rtl,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
@@ -72,26 +75,50 @@ class MySelectedPackage extends State<SelectedPackage> {
                 overflow: TextOverflow.clip,
                 textAlign: TextAlign.center,
               ),
-              Flexible(child:  ListView.builder(padding: EdgeInsets.all(5.0),itemCount: widget.packageBoxList.length,scrollDirection: Axis.vertical,
+              Flexible(
+                  child: ListView.builder(
+                padding: EdgeInsets.all(5.0),
+                itemCount: widget.packageBoxList.length,
+                scrollDirection: Axis.vertical,
                 itemBuilder: (BuildContext context, int index) {
                   return new GestureDetector(
-                    onTap: () {},
-                    child: Row(
-                      children: <Widget>[
-                        Text(widget.packageBoxList[index].title,style: TextStyle(fontSize: 25.0),)
-                      ],
-
-                    ),
-                  );
+                      onTap: () {
+                        BoxState = widget.packageBoxList[index].boxState;
+                        switch (BoxState) {
+                          case 0:
+                            Text("you dont Own this Box");
+                            break;
+                          case 1:
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => LearningPage(box: widget.packageBoxList[index],)));
+                            break;
+                          case 2:
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => ExaminingPage()));
+                            break;
+                          case 3:
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => ReviewingPage()));
+                            break;
+                          default:
+                            Text("Wrong chiz");
+                        }
+                      },
+                      child: Row(children: <Widget>[
+                        widget.packageBoxList[index].GetBoxIcon(),
+                        Text(widget.packageBoxList[index].title,
+                            style: TextStyle(fontSize: 25.0))
+                      ]));
                 },
               ))
             ],
           )),
     );
   }
-
-
-
-
-
 }
