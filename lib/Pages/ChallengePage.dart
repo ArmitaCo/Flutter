44055b8e2e5c57c2b1c99a29162c712e8fa4e0e5
Mohsen Meshkini <mmeshkiny@yahoo.com>
2018-11-14 15:17:04 +0330

    Challenge page graphic improvment

--- lib/Pages/ChallengePage.dart
+++ lib/Pages/ChallengePage.dart
@@ -19,6 +19,7 @@ class ChallengePageState extends State<ChallengePage> {
   @override
   void initState() {
     super.initState();
+
     getLeaderBoard(context).then((kh) {
       setState(() {
         model = kh;
@@ -30,9 +31,33 @@ class ChallengePageState extends State<ChallengePage> {
           fit: BoxFit.fitWidth,
         );
         itemsCount = model.leaderBoardItems.length;
+        List<TableRow> rows =
+            model.leaderBoardItems.map((lbi) => _getTableRow(lbi)).toList();
+        rows.insert(
+            0,
+            TableRow(
+                children: <Widget>[
+                  Text(
+                    "رتبه",
+                    textScaleFactor: 1.4,
+                    textAlign: TextAlign.center,
+                  ),
+                  Padding(
+                      padding: EdgeInsets.only(right: 5.0),
+                      child: Text("نام", textScaleFactor: 1.4)),
+                  Text("امتیاز",
+                      textScaleFactor: 1.4, textAlign: TextAlign.center),
+                ],
+                decoration: BoxDecoration(
+                    border: Border(
+                        bottom: BorderSide(
+                            style: BorderStyle.solid,
+                            color: MyColors.borders,
+                            width: 1.0)))));
         table = Table(
-          children:
-              model.leaderBoardItems.map((lbi) => _getTableRow(lbi)).toList(),
+//          border: TableBorder.all(
+//              color: Colors.black, width: 1.0, style: BorderStyle.solid),
+          children: rows,
           columnWidths: const <int, TableColumnWidth>{
             0: FlexColumnWidth(0.1),
             1: FlexColumnWidth(0.7),
@@ -46,14 +71,16 @@ class ChallengePageState extends State<ChallengePage> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-        floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-        floatingActionButton: FloatingActionButton(
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButton: FloatingActionButton(
         onPressed: () {},
-    child: Text("راهنما"),mini: true,),
+        child: Text("راهنما"),
+        mini: true,
+      ),
       backgroundColor: MyColors.firstBackground,
       body: SingleChildScrollView(
           child: Column(
-        children: <Widget>[upperImage, countDownWidget, table],
+        children: <Widget>[upperImage, countDownWidget, Divider(), table],
       )),
     );
   }
@@ -65,13 +92,21 @@ class ChallengePageState extends State<ChallengePage> {
         child: Text(
           item.order.toString(),
           textScaleFactor: 1.2,
+          textAlign: TextAlign.center,
         )));
     widgetList.add(Padding(
-        padding: EdgeInsets.all(5.0),
+        padding: EdgeInsets.only(
+          bottom: 5.0,
+          right: 5.0,
+        ),
         child: Text(item.name, textScaleFactor: 1.2)));
     widgetList.add(Padding(
         padding: EdgeInsets.all(5.0),
-        child: Text(item.score.toString(), textScaleFactor: 1.2)));
+        child: Text(
+          item.score.toString(),
+          textScaleFactor: 1.2,
+          textAlign: TextAlign.center,
+        )));
     return TableRow(children: widgetList);
   }
 }
